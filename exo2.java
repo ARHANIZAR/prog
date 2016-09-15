9150bb6bd7318ea21e007b867632d01fbd8069bf
Aymane RHANIZAR <aymane.rhanizar@edu.univ-fcomte.fr>
2016-09-15 15:06:30 +0200

    correcrtion exo 2

--- exo2.java
+++ exo2.java
@@ -11,9 +11,9 @@ class exo2{
 		File file;
 		int lastIndex;
 
-		if ( args.length == 0 ){
+		if ( args.length < 2 ){
 			System.out.println("set valid parameters: java exo2 [file name] [the word to find] ");
-			return;
+			System.exit(1);
 		}
 
 		FILE_NAME = args[0];
@@ -21,18 +21,27 @@ class exo2{
 		
 		file = new File(FILE_NAME);
 
-		if(!file.exists()){
-			System.out.println("File does not exist !!");
-			return;
+		try {
+			if(!file.exists()){
+				System.out.println("File does not exist !!");
+				System.exit(1);
+			}
+		} catch(SecurityException e){
+			System.out.println("Err exits() method : " + e.getMessage());
 		}
+
+
 		try {
 
 			br = new BufferedReader(new FileReader(file));
+			boolean stop = false;
 
 			do {
 				str = br.readLine();
-				str = (str == null) ?  "" : str;
-				//System.out.println(str);
+				if (str == null){
+					stop = true;
+					str = "";
+				}
 				lastIndex = 0;
 				while (lastIndex != -1){
 					lastIndex = str.indexOf(WORD_TO_FIND,lastIndex);
@@ -43,7 +52,7 @@ class exo2{
 					}
 				}
 
-			}while ( !str.equals("") && str != null);
+			}while (!stop);
 			br.close();
 
 			System.out.println("the file " + FILE_NAME + "has " + count + " occurence of the word " + WORD_TO_FIND);
